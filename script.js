a487c6cb606ff4156be08336414a88103a0b4a1a
Luiz Ricardo Silva <ricardo.rs95@gmail.com>
2020-06-13 16:06:42 -0300

    finalizando modal a animação de transiçao

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-document
-            .querySelector("button.fat")
-            .addEventListener("click", function(){
-                document
-                    .querySelector("#footer")
-                    .classList
-                    .toggle("hide")
-                })
+function onOff() {
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+}
+
+document.querySelector("#modal a")
+    .addEventListener("click", onOff)
